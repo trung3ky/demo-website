48fc4bd0ccd0e0183c84dfca5d4a41cbb668a2ff
Trung Huynh <trunghuynh@Trungs-Mac-mini.lan>
2026-06-11 00:19:18 +0700

    update:2

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const nextConfig: NextConfig = {
   turbopack: {
-    root: resolve(__dirname),
+    root: import.meta.dirname,
   },
 };
 
 export default nextConfig;
+
